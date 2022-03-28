4191ffdd155f77bab56b1ea79d9d7b40e5023c31
IATec - Filipe Nogueira <filipe.nogueira@iatec.com>
2022-03-28 12:40:00 -0300

    Implementando o contrato com a Interface

---
+++ src/DevIO.Business/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,16 @@
+﻿using DevIO.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Interfaces
+{
+    interface IProdutoRepository : IRepository<Produto>
+    {
+        Task<IEnumerable<Produto>> ObterProdutosPorFornecedore(Guid FornecedorId);
+        Task<IEnumerable<Produto>> ObterProdutosFornecedores();
+        Task<Produto> ObterProdutoFornecedor(Guid id);
+    }
+}
---
+++ src/DevIO.Data/Repository/Repository.cs
@@ -0,0 +1,58 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using DevIO.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Data.Repository
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity
+    {
+        protected readonly MeuDbContext Db;
+        protected readonly DbSet<TEntity> DbSet;
+        public async Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await DbSet.AsNoTracking().Where(predicate).ToListAsync();
+        }
+
+        public async Task<TEntity> ObterPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<TEntity>> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Adicionar(TEntity entity)
+        {
+            DbSet.Add(entity);
+            await SaveChanges();
+        }
+
+        public async Task Atualizar(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+        public async void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
