a1256a06fd783af50d30c3d4ac1bdca8d1dddcdd
IATec - Filipe Nogueira <filipe.nogueira@iatec.com>
2022-03-30 15:43:51 -0300

    Estilizando e implementando view de produtos e fornecedores

--- src/DevIO.App/ViewModels/ProdutoViewModel.cs
+++ src/DevIO.App/ViewModels/ProdutoViewModel.cs
@@ -25,7 +25,7 @@ namespace DevIO.App.ViewModels
         [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Descricao { get; set; }
 
-        [NotMapped]
+        [DisplayName("Imagem do Produto")]
         public IFormFile ImagemUpload { get; set; }
 
         public string Imagem { get; set; }
@@ -39,6 +39,7 @@ namespace DevIO.App.ViewModels
         [DisplayName("Ativo?")]
         public bool Ativo { get; set; }
         public FornecedorViewModel Fornecedor { get; set; }
+
         public IEnumerable<FornecedorViewModel> Fornecedores { get; set; }
     }
 }
